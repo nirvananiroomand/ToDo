55b40b80fda5b41f0d9aa07d70489fedc8997367
nirvananiroomand <nirvana.niroomand@gmail.com>
2022-11-09 12:10:04 +0330

    some style changes

---
+++ todo.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () =>{
+    const form = document.querySelector('form');
+    // const input = document.querySelector()
+})
+const form = document.querySelector('form');
+console.log(form);
